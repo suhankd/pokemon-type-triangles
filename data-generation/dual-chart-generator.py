822198eeb1016947f8796eba8f0a943389b6ed45
suhankd <suhanraodonthineni@gmail.com>
2026-01-13 19:40:25 +0530

    added dual type chart

---
+++ data-generation/dual-chart-generator.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import itertools
+
+mono_chart = pd.read_csv(f"./data/mono-chart.csv")
+mono_chart.set_index("Attacking", inplace = True)
+
+types = mono_chart.index.tolist()
+
+dual_types = list(itertools.combinations(types, 2))
+
+mono_vs_dual = pd.DataFrame(
+    index = types,
+    columns = [f"{t1}/{t2}" for t1, t2 in dual_types],
+    dtype = float
+)
+
+for attacker in types:
+
+    for t1, t2 in dual_types:
+
+        mono_vs_dual.loc[attacker, f"{t1}/{t2}"] = (mono_chart.loc[attacker, t1] * mono_chart.loc[attacker, t2])
+
+# Saving the mono-dual chart.
+
+mono_vs_dual.index.name = "Attacking"
+mono_vs_dual.to_csv("./data/mono-dual-chart.csv")
+
+dual_vs_dual = pd.DataFrame(
+
+    index = [f"{t1}/{t2}" for t1, t2 in dual_types],
+    columns = [f"{t1}/{t2}" for t1, t2 in dual_types],
+    dtype = float
+
+)
+
+for a1, a2 in dual_types:
+
+    attacker = f"{a1}/{a2}"
+
+    for b1, b2 in dual_types:
+
+        defender = f"{b1}/{b2}"
+
+        dual_vs_dual.loc[attacker, defender] = (mono_vs_dual.loc[a1, defender] + mono_vs_dual.loc[a2, defender])/2
+
+dual_vs_dual.index.name = "Attacking"
+dual_vs_dual.to_csv("data/dual-chart.csv")
